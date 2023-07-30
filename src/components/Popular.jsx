895de2ac8d03ce53e2d125df35786a187dd4969e
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-07-31 01:00:18 +0530

    fetching popular recipe details from api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Popular = () => {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    const getPopular = async () => {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      setPopular(data.recipes);
+    };
+    getPopular();
+  }, []);
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <h1>{recipe.title}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Popular;
